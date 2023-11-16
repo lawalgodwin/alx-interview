a0e64b08d117223dc5f596081fa083842c30b4df
lawalgodwin <godwinl200@gmail.com>
2023-11-16 09:59:23 +0100

    Rotate 2D matrix 90 degree clock-wise (in-place)

---
+++ 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""A module containing the logic for retating a 2D Matrix"""
+
+
+def rotate_2d_matrix(matrix):
+    """Rotate a 2D matrix 90 degree clock-wise"""
+    # Transpose the matrix
+    for i in range(len(matrix)):
+        for j in range(i, len(matrix)):
+            matrix[j][i], matrix[i][j] = matrix[i][j], matrix[j][i]
+    # Reverse the order of each row in the transposed matrix
+    for i in range(len(matrix) - 1):
+        for j in range(i, len(matrix)):
+            matrix[j][i], matrix[j][len(matrix) - 1 - i] =\
+                matrix[j][len(matrix) - 1 - i], matrix[j][i]
