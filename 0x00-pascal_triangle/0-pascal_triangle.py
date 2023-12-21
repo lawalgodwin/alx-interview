4e4c7544413d6bf57dc7435e3c2a0ca813bb993c
lawalgodwin <godwinl200@gmail.com>
2023-12-21 10:50:00 +0100

    Add a Pascal Triangle generator

---
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""A module that contains a pascal triangle generation function"""
+
+
+def pascal_triangle(n):
+    """Pascal Triangle Generator"""
+    if n <= 0:
+        return []
+    triangle = [[1]]
+    for i in range(1, n):
+        row = [1]
+        for j in range(1, i):
+            row.append(triangle[i - 1][j - 1] + triangle[i - 1][j])
+        row.append(1)
+        triangle.append(row)
+    return triangle
